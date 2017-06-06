39e75a0ce8f969b0aefc17f0a1a184dbb1771ec7
RM Terrell <RM-Terrell@users.noreply.github.com>
2017-06-06 11:40:38 -0600

    Fixed table row addition styling and table resizing

--- mracalc.js
+++ mracalc.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
         this.selectedCiValue = ko.observable();
 
         this.ciSdResult = ko.computed(
-               function () {
-                   return Math.round(((this.upperBound() - this.lowerBound()) * Math.sqrt(this.ciN()) / (2 * this.selectedCiValue()))* 100) / 100
-               }, this);
+            function () {
+                return Math.round(((this.upperBound() - this.lowerBound()) * Math.sqrt(this.ciN()) / (2 * this.selectedCiValue())) * 100) / 100
+            }, this);
 
 
     }
@@ -56,63 +56,65 @@ function deleteRow(row) {
     var rowCount = $('#InputTable tr').length;
     if (rowCount > 2) {
 
-        document.getElementById('InputTable').deleteRow(rowCount - 1);
+        (<HTMLTableElement>document.getElementById('InputTable')).deleteRow(rowCount - 1);
     }
 
- }
+}
 
 // Row Insertion for Dynamic Table//
 function insRow() {
-  var x = document.getElementById('InputTable');
-  // copy the targeted row
-  var new_row = x.rows[1].cloneNode(true);
-  // get the total number of rows
-  var len = x.rows.length;
-  // set the innerHTML of the first row
-  new_row.cells[0].innerHTML = len;
+    var x = <HTMLTableElement>(document.getElementById('InputTable'));
+    // copy the targeted row
+    var new_row = <HTMLTableRowElement>(x.rows[1].cloneNode(true));
+    // get the total number of rows
+    var len = x.rows.length.toString();
+    // set the innerHTML of the first row
+    new_row.cells[0].innerHTML = len;
 
-  // grab the input from the first cell and update its ID and value
-  var inp1 = new_row.cells[1].getElementsByTagName('input')[0];
-  inp1.id += len;
-  inp1.value = '';
+    var AppendTarget = <HTMLTableElement>(document.getElementById('AppendTarget'));
 
-  // append the new row to the table
-  x.appendChild(new_row);
+    // grab the input from the first cell and update its ID and value
+    var inp1 = new_row.cells[1].getElementsByTagName('input')[0];
+    inp1.id += len;
+    inp1.value = '';
+
+    // append the new row to the table
+    AppendTarget.appendChild(new_row);
 } /*Origin of source material for row addition and deletion function "http://stackoverflow.com/questions/6473111/add-delete-table-rows-dynamically-using-javascript" */
 
 
 function calculateMSD() {
 
-//------------------------------- Mean Calculations--------------------------------------//
-  var sum = 0;
-  var rowCount = $('#InputTable tr').length;
-  //iterate through each textboxes and add the values
-  $(".PatientV").each(function() {
+    //------------------------------- Mean Calculations--------------------------------------//
+    var sum = 0;
+    var rowCount = $('#InputTable tr').length;
+    //iterate through each textboxes and add the values
+    $(".PatientV").each(function () {
 
-    //add only if the value is number
-    if (!isNaN(this.value) && this.value.length != 0) {
-      sum += parseFloat(this.value);
-    }
+        //add only if the value is number
+        if (!isNaN(this.value) && this.value.length != 0) {
+            sum += parseFloat(this.value);
+        }
 
-  });
-  var totalMean = sum / (rowCount - 1);
+    });
+    var totalMean = sum / (rowCount - 1);
 
-  $("#totalMean").val(totalMean.toFixed(2));
+    $("#totalMean").val(totalMean.toFixed(2));
 
-//--------------------------------- SD Calculations---------------------------------//
+    //--------------------------------- SD Calculations---------------------------------//
 
-  var SqrDiffSum = 0;
+    var SqrDiffSum = 0;
 
-  $(".PatientV").each(function () {
+    $(".PatientV").each(function () {
 
-      //add only if the value is number
-      if (!isNaN(this.value) && this.value.length != 0) {
-          SqrDiffSum += Math.pow((parseFloat(this.value) - totalMean), 2);
-      }
+        //add only if the value is number
+        if (!isNaN(this.value) && this.value.length != 0) {
+            SqrDiffSum += Math.pow((parseFloat(this.value) - totalMean), 2);
+        }
 
-  });
+    });
 
-  var totalSD = Math.sqrt(SqrDiffSum / (rowCount - 1));
-  $("#totalSD").val(totalSD.toFixed(2));
+    var totalSD = Math.sqrt(SqrDiffSum / (rowCount - 1));
+    $("#totalSD").val(totalSD.toFixed(2));
 
 }
